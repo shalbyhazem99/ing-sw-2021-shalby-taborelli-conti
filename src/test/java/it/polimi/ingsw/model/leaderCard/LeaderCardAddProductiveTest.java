a217a08f6897eacc2ba0ba9b604fa9e5786b2a43
stefano taborelli <10608112@polimi.it>
2021-04-28 12:21:18 +0200

    Test classes v3

---
+++ src/test/java/it/polimi/ingsw/model/leaderCard/LeaderCardAddProductiveTest.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.model.leaderCard;
+
+import it.polimi.ingsw.model.*;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LeaderCardAddProductiveTest extends TestCase {
+
+    /*
+       public boolean active(Player player) {
+        if(!active && isActivable(player)) { //if not active and it's not activable
+            player.addPower(ProductivePower.getInstance(
+                    new ArrayList<>(Arrays.asList(ResourcesCount.getInstance(1, resourceTypeRelated))),//todo: to see
+                    new ArrayList<>(Arrays.asList(
+                            Resource.getInstance(ResourceType.ANY), //todo: to see
+                            Resource.getInstance(ResourceType.FAITH))))
+            );
+            return true;
+        }
+        return false;
+    }
+     */
+    public void testActive() {
+    }
+}
