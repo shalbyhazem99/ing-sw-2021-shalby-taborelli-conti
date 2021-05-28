8f90c95228f5cf219fd83491fbe3acc712f2404e
hazem <shalbyhazem99@hotmail.com>
2021-05-28 03:14:07 +0200

    gui without reflection

---
+++ src/main/java/it/polimi/ingsw/HelloFXLauncher.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw;
+
+
+import it.polimi.ingsw.gui.HelloFX;
+
+public class HelloFXLauncher {
+    public static void main(String[] args) {
+        HelloFX.main(args);
+    }
+}
